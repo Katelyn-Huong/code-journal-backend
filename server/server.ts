5fa93c7138d66030e06dfe572e63b34a772c1821
Katelyn Ngo <thienhuongngo13@gmail.com>
2024-07-26 18:09:35 +0000

    added catch error statement

--- server/server.ts
+++ server/server.ts
@@ -4,6 +4,13 @@ import pg from 'pg';
 import express from 'express';
 import { ClientError, errorMiddleware } from './lib/index.js';
 
+export type Entry = {
+  entryId?: number;
+  title: string;
+  notes: string;
+  photoUrl: string;
+};
+
 const db = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
@@ -12,6 +19,44 @@ const db = new pg.Pool({
 });
 
 const app = express();
+app.use(express.json());
+
+app.get('/api/entries', async (req, res, next) => {
+  try {
+    const sql = `
+    select * from "entries";`;
+
+    const result = await db.query<Entry>(sql);
+    const entries = result.rows;
+    res.json(entries);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get('/api/entries/:entryId', async (req, res, next) => {
+  try {
+    const { entryId } = req.params;
+    if (!Number.isInteger(+entryId)) {
+      throw new ClientError(400, 'entryId must be an integer');
+    }
+    const sql = `
+    select "entryId", "title", "notes", "photoUrl"
+    from "entries"
+    where "entryId" = $1;
+    `;
+    const params = [entryId];
+
+    const result = await db.query<Entry>(sql, params);
+    const [entry] = result.rows;
+    if (!entry) throw new ClientError(404, 'invalid entryId');
+    res.json(entry);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.use(errorMiddleware);
 
 app.listen(process.env.PORT, () => {
   console.log(`express server listening on port ${process.env.PORT}`);
