f18a4f5638d5a1fa9f349bb8d443784ea63294a3
Mark Matlock <techmatlock@gmail.com>
2024-07-26 18:56:10 +0000

    Added update route for server

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars -- Remove me */
 import 'dotenv/config';
-import pg from 'pg';
+import pg, { Client } from 'pg';
 import express from 'express';
 import { ClientError, errorMiddleware } from './lib/index.js';
 
@@ -49,8 +49,57 @@ app.get('/api/entries/:entryId', async (req, res, next) => {
 
     const result = await db.query<Entry>(sql, params);
     const [entry] = result.rows;
-    if (!entry) throw new ClientError(404, 'invalid entryId');
+    if (!entry) throw new ClientError(404, `Entry ${entryId} not found.`);
     res.json(entry);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.post('/api/entries', async (req, res, next) => {
+  try {
+    const { title, photoUrl, notes } = req.body;
+    if (!title || !photoUrl || !notes) {
+      throw new ClientError(400, 'title, photoURL, or notes is required');
+    }
+
+    const sql = `
+      insert into "entries" ("title", "photoUrl", "notes")
+        values ($1, $2, $3)
+        returning *;
+    `;
+    const params = [title, photoUrl, notes];
+    const result = await db.query<Entry>(sql, params);
+    const [entry] = result.rows;
+    res.status(201).json(entry);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.put('/api/entries/:entryId', async (req, res, next) => {
+  try {
+    const { entryId } = req.params;
+    if (!Number.isInteger(+entryId))
+      throw new ClientError(400, `entryId ${entryId} must be a number`);
+    const { title, photoUrl, notes } = req.body;
+    if (!title || !photoUrl || !notes)
+      throw new ClientError(400, 'title, photo, and notes are required.');
+
+    const sql = `
+      update "entries"
+        set "title" = $1,
+            "photoUrl" = $2,
+            "notes" = $3
+        where "entryId" = $4
+        returning *;
+    `;
+
+    const params = [title, photoUrl, notes, entryId];
+    const result = await db.query<Entry>(sql, params);
+    const [entry] = result.rows;
+    if (!entry) throw new ClientError(404, 'Failed to update entry.');
+    res.status(200).json(entry);
   } catch (err) {
     next(err);
   }
